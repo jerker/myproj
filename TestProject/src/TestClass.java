180da647fd585f3d74046ff2cdce4b1bd0062038
Jerker Skogby <jskogby@kth.se>
2009-03-15 18:29:13 +0100

    Added a double function... Just recommiting..

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -2,7 +2,9 @@
 public class TestClass {
 	/* Adding a few lines to class... */
 	private int integer = 0;
+	private double doubleNr = 0.09;
 	
 	public int getInteger() { return integer; }
-
+	public double getDoubleNr(){ return doubleNr; }
+	
 }
